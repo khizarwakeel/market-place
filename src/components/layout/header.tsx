7cafe94b8021b095aaa0c54900429fb1185e1096
Khizar Wakeel <khizarwakeel@gmail.com>
2024-01-05 21:20:15 +0500

    added some changes

--- src/components/layout/header.tsx
+++ src/components/layout/header.tsx
@@ -1,10 +1,24 @@
-import Logo from "/assets/logo.webp";
+import Logo from "/public/assets/Logo.webp";
+import Image from "next/image";
 
 const Header = () => {
   return (
     <section>
-        <div>
-          <h1>Header</h1>
+        <div className="flex">
+          <div>
+          <Image src={Logo} alt="O Dine Market" />
+          </div>
+          <div>
+            <ul className="flex gap-10">
+              <li>Female</li>
+              <li>Male</li>
+              <li>Kids</li>
+              <li>All Products</li>
+            </ul>
+          </div>
+          <div>
+            <input type="text" name="" id="" placeholder="Enter " />
+          </div>
         </div>
     </section>
   );
