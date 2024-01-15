9bc0962b4e091e7c4704728ba3beffa3e692efc8
Khizar Wakeel <khizarwakeel@gmail.com>
2024-01-15 19:47:48 +0500

    added new data file

---
+++ src/components/utils/data.tsx
@@ -0,0 +1,62 @@
+import { StaticImageData } from "next/image";
+import ProductImg1 from "/public/assets/girl1.png";
+import ProductImg2 from "/public/assets/girl2.png";
+import ProductImg3 from "/public/assets/boy1.webp";
+import ProductImg4 from "/public/assets/boy2.webp";
+import ProductImg5 from "/public/assets/kids1.jpg";
+import ProductImg6 from "/public/assets/kids2.webp";
+
+// Product Data
+
+type Product = {
+  id: number;
+  name: string;
+  price: number;
+  category: string;
+  image: string | StaticImageData;
+};
+
+export const products: Product[] = [
+  {
+    id: 1,
+    name: "Brushed Raglan Sweatshirt",
+    category: "Female",
+    price: 195,
+    image: ProductImg1,
+  },
+  {
+    id: 2,
+    name: "Cameryn Sash Tie Dress",
+    category: "Female",
+    price: 545,
+    image: ProductImg2,
+  },
+  {
+    id: 3,
+    name: "Shirts",
+    category: "Kids",
+    price: 545,
+    image: ProductImg5,
+  },
+  {
+    id: 4,
+    name: "Kids Dress",
+    category: "Kids",
+    price: 720,
+    image: ProductImg6,
+  },
+  {
+    id: 5,
+    name: "Flex Push Button Bomber",
+    category: "Male",
+    price: 225,
+    image: ProductImg3,
+  },
+  {
+    id: 6,
+    name: "Raglan Sweatshirt",
+    category: "Male",
+    price: 195,
+    image: ProductImg4,
+  },
+];
--- src/components/views/productsList.tsx
+++ src/components/views/productsList.tsx
@@ -1,9 +1,6 @@
-import ProductCards from "../shared/productCards";
+import Image from "next/image";
 import Wrapper from "../shared/wrapper";
-import ProductImg1 from "/public/assets/feature.webp";
-import ProductImg2 from "/public/assets/feature2.png";
-import ProductImg3 from "/public/assets/feature3.png";
-import ProductImg4 from "/public/assets/feature4.png";
+import { products } from "../utils/data";
 
 const ProductsList = () => {
   return (
@@ -14,18 +11,19 @@ const ProductsList = () => {
           <h1 className="text-4xl font-bold py-4 pb-5">Check What We Have</h1>
         </div>
         <div className="grid grid-cols-4 gap-5">
-          <ProductCards
-            img={ProductImg1}
-            title="Imperial Alpaca Hoodie"
-            price={525}
-          />
-          <ProductCards
-            img={ProductImg2}
-            title="Brushed Raglan Sweatshirt"
-            price={195}
-          />
-          <ProductCards img={ProductImg3} title="Brushed Bomber" price={225} />
-          <ProductCards img={ProductImg4} title="Muscle Tank" price={75.0} />
+          {products.slice(0, 4).map((item, index) => (
+            <div key={index}>
+              <div className="py-5 hover:scale-105 duration-300">
+                <Image
+                  src={item.image}
+                  alt="Products"
+                  className="object-cover flex-shrink-0"
+                />
+                <h3 className="py-2 font-bold">{item.name}</h3>
+                <p className="text-gray-500">${item.price}</p>
+              </div>
+            </div>
+          ))}
         </div>
       </Wrapper>
     </section>
